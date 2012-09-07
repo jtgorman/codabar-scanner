3b761146e3e5296a6549e071f4bf739fe75edbb7
Jon Gorman <jonathan.gorman@gmail.com>
2012-09-06 21:16:16 -0500

    re-enabled codabar, disabled other formats in MultiformatOneDReader.java.  Bare minimum to get barcode scanner android app to recognize codabar (and only codabar).

--- zxing-2.0/core/src/com/google/zxing/oned/MultiFormatOneDReader.java
+++ zxing-2.0/core/src/com/google/zxing/oned/MultiFormatOneDReader.java
@@ -45,43 +45,43 @@ public final class MultiFormatOneDReader extends OneDReader {
         hints.get(DecodeHintType.ASSUME_CODE_39_CHECK_DIGIT) != null;
     Collection<OneDReader> readers = new ArrayList<OneDReader>();
     if (possibleFormats != null) {
-      if (possibleFormats.contains(BarcodeFormat.EAN_13) ||
-          possibleFormats.contains(BarcodeFormat.UPC_A) ||
-          possibleFormats.contains(BarcodeFormat.EAN_8) ||
-          possibleFormats.contains(BarcodeFormat.UPC_E)) {
-        readers.add(new MultiFormatUPCEANReader(hints));
-      }
-      if (possibleFormats.contains(BarcodeFormat.CODE_39)) {
-        readers.add(new Code39Reader(useCode39CheckDigit));
-      }
-      if (possibleFormats.contains(BarcodeFormat.CODE_93)) {
-        readers.add(new Code93Reader());
-      }
-      if (possibleFormats.contains(BarcodeFormat.CODE_128)) {
-        readers.add(new Code128Reader());
-      }
-      if (possibleFormats.contains(BarcodeFormat.ITF)) {
-         readers.add(new ITFReader());
-      }
+      // if (possibleFormats.contains(BarcodeFormat.EAN_13) ||
+      //     possibleFormats.contains(BarcodeFormat.UPC_A) ||
+      //     possibleFormats.contains(BarcodeFormat.EAN_8) ||
+      //     possibleFormats.contains(BarcodeFormat.UPC_E)) {
+      //   readers.add(new MultiFormatUPCEANReader(hints));
+      // }
+      // if (possibleFormats.contains(BarcodeFormat.CODE_39)) {
+      //   readers.add(new Code39Reader(useCode39CheckDigit));
+      // }
+      // if (possibleFormats.contains(BarcodeFormat.CODE_93)) {
+      //   readers.add(new Code93Reader());
+      // }
+      // if (possibleFormats.contains(BarcodeFormat.CODE_128)) {
+      //   readers.add(new Code128Reader());
+      // }
+      // if (possibleFormats.contains(BarcodeFormat.ITF)) {
+      //    readers.add(new ITFReader());
+      // }
       if (possibleFormats.contains(BarcodeFormat.CODABAR)) {
          readers.add(new CodaBarReader());
       }
-      if (possibleFormats.contains(BarcodeFormat.RSS_14)) {
-         readers.add(new RSS14Reader());
-      }
-      if (possibleFormats.contains(BarcodeFormat.RSS_EXPANDED)){
-        readers.add(new RSSExpandedReader());
-      }
+      // if (possibleFormats.contains(BarcodeFormat.RSS_14)) {
+      //    readers.add(new RSS14Reader());
+      // }
+      // if (possibleFormats.contains(BarcodeFormat.RSS_EXPANDED)){
+      //   readers.add(new RSSExpandedReader());
+      // }
     }
     if (readers.isEmpty()) {
-      readers.add(new MultiFormatUPCEANReader(hints));
-      readers.add(new Code39Reader());
-      //readers.add(new CodaBarReader());
-      readers.add(new Code93Reader());
-      readers.add(new Code128Reader());
-      readers.add(new ITFReader());
-      readers.add(new RSS14Reader());
-      readers.add(new RSSExpandedReader());
+        //readers.add(new MultiFormatUPCEANReader(hints));
+        //readers.add(new Code39Reader());
+      readers.add(new CodaBarReader());
+      //readers.add(new Code93Reader());
+      //readers.add(new Code128Reader());
+      //readers.add(new ITFReader());
+      //readers.add(new RSS14Reader());
+      //readers.add(new RSSExpandedReader());
     }
     this.readers = readers.toArray(new OneDReader[readers.size()]);
   }
